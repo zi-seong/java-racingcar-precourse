f698a4e9290a982d2c4faae80db56d30949263fd
zi-seong <jiseong4577@gmail.com>
2022-10-10 02:45:22 +0900

    feat : Car를 List로 관리하는 Cars생성
    Cars클래스를 사용하여 Car클래스를 편하게 관리하고 우승자도 뽑을 수 있게 구현하기

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> carList = new ArrayList<>();
+
+
+    public Cars(String cars) {
+        //TODO: 밸리데이션 체크하기
+        this.carList = carList;
+    }
+
+    //TODO: 우승자를 뽑는 메소드 구현하기
+}
