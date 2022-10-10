e82dba60f355ff0865ea3027731895d3263bf9fa
zi-seong <jiseong4577@gmail.com>
2022-10-10 17:13:50 +0900

    feat : 레이싱에 참가하는 자동차 목록, 시도할 회수가 들어가는 RacingSetting 추가

--- src/main/java/racingcar/domain/RacingSetting.java
+++ src/main/java/racingcar/domain/RacingSetting.java
@@ -6,14 +6,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RacingSetting {
 
 
-    public static final int MIN_RANDOM_NUM = 0;
-    public static final int MAX_RANDOM_NUM = 9;
 
     private Cars cars = new Cars();
 
     private int round = 0;
 
-    public void racingSetting(){
+    public  RacingSetting(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분");
         this.cars = new Cars(Console.readLine());
         System.out.println("시도할 회수는 몇회인가요?");
@@ -22,12 +20,13 @@ public class RacingSetting {
         this.round = Integer.parseInt(roundNum);
     }
 
-
-    public int getRandomNumber(){
-        return Randoms.pickNumberInRange(MIN_RANDOM_NUM,MAX_RANDOM_NUM);
+    public Cars getCars() {
+        return cars;
     }
 
-
+    public int getRound(){
+        return round;
+    }
 
     private void validateRoundNum(String roundNum) {
         try{
